9d322e0cc840272a915ab53f29ab303063127e3b
Arduino Fikirleri <92206667+arduinofikirleri@users.noreply.github.com>
2026-04-04 18:46:33 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'buradayim-v1';
+const assets = [
+  '/',
+  '/index.html',
+  'https://i.ibb.co/HLYFNzSD/onboarding.jpg',
+  'https://i.ibb.co/FLqNgT9P/kirikkalp1.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
